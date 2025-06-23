04c1286f59e1ecd4f0c884afc2f76b7cfb21393a
Katherine Kurtanidze <kek@vava.cars>
2025-06-23 23:01:54 +0200

    Increase wait time

--- worker/src/deploymentProcessor.ts
+++ worker/src/deploymentProcessor.ts
@@ -225,7 +225,7 @@ export class DeploymentProcessor {
       setTimeout(() => {
         sstProcess.kill('SIGKILL');
         reject(new Error('Deployment timeout after 20 minutes'));
-      }, 20 * 60 * 1000);
+      }, 40 * 60 * 1000);
     });
   }
 
